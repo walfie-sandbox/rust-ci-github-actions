9bfd32e0f5c8d062ccfc6a2f90c216cd08c148e1
Walfie <walfington@gmail.com>
2021-09-08 14:57:30 -0400

    Add HTTPS request for more realistic usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let resp = reqwest::get("https://httpbin.org/anything")
+        .await
+        .expect("request failed")
+        .text()
+        .await
+        .expect("failed to read response body as string");
+
+    println!("{}", resp);
 }
